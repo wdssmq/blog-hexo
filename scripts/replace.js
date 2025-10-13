097f70e4e249acf5d0f8951730a4c412f449a03f
wdssmq <wdssmq@qq.com>
2025-10-14 01:58:10 +0800

    up. 扩展脚本

--- scripts/replace.js
+++ scripts/replace.js
@@ -1,8 +1,22 @@
-hexo.extend.filter.register('after_post_render', function (data) {
+hexo.extend.filter.register('after_post_render', function(data) {
     // 图片路径替换
     const imgHost = "https://img.wdssmq.com/";
     const imgUrl = `https://img.wdssmq.com/${data.source.replace("doc.md", "")}`;
     data.content = data.content.replace(/<img src="\/\.\//g, `<img src="${imgHost}/_posts/`);
     data.content = data.content.replace(/<img src="\//g, `<img src="${imgUrl}`);
+    data.content = data.excerpt + data.content;
+
+    // let result = '';
+    // for (let key in data) {
+    //     if (data.hasOwnProperty(key)) {
+    //         if (/raw|_content|content|more|excerpt/.test(key)) continue;
+    //         result += key + ':' + data[key] + '\n\n';
+    //     }
+    // }
+
+    // permalink 中替换 blog. 为 www. 
+    const wwwLink = data.permalink.replace("blog.", "www.");
+    data.content = `<blockquote><p>本文标题：《${data.title}》。</p><p>镜像链接：<a href="${wwwLink}" title="${data.title}">${wwwLink}</a></p></blockquote>\n\n` + data.content;
+
     return data;
 });
